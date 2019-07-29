cf2d56bc19e514df9cdaad524c4530ef3ca52427
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 23:28:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,6 @@
 def sort_array_asc(int)
-  int.sort
+  int.sort do |a, b|
+  a <=> b
 end
 
 def sort_array_desc(int)
@@ -9,5 +10,6 @@ end
 end
 
 def sort_array_char_count(int)
-  
+  int.sort do |a.length, b.length|
+    a 
 end
